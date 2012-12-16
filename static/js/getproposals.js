44341808d2085bb7b403a17bd0d96176c7b21973
muki <filipdobranic@gmail.com>
2012-12-16 18:02:34 +0100

    fixed list_proposals.php

--- static/js/getproposals.js
+++ static/js/getproposals.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-
-	$.getJSON('http://sect.io/danesjenovdan/ajax/list_proposals.php', function(data) {
+	var url = 'http://sect.io/danesjenovdan/ajax/list_proposals.php?id=' + $('#rightid').val();
+	$.getJSON(url, function(data) {
 		for (var i=0; i<data.length; i++) {
-			$('#predlogi').append('<div class="row predlog"><div class="span4"><p class="suggestiontitle">'+ data[i].title +'</p></div><div class="span1"><p class="timestamp">'+data[i].timestamp+'</p></div><div class="span4"><div class="votebox"><div class="votefor">glasujza '+data[i].vote_plus+'</div><div class="voteagainst">glasujproti '+data[i].vote_minus+'</div></div></div><div class="span3"><div class="ihaveanargument">imam argument</div></div></div>');
+			$('#predlogi').prepend('<div class="row predlog"><div class="span4"><p class="suggestiontitle">'+ data[i].title +'</p></div><div class="span1"><p class="timestamp">'+data[i].timestamp+'</p></div><div class="span4"><div class="votebox"><div class="votefor">glasujza '+data[i].vote_plus+'</div><div class="voteagainst">glasujproti '+data[i].vote_minus+'</div></div></div><div class="span3"><div class="ihaveanargument">imam argument</div></div></div>');
 		}
 	});	
 });
