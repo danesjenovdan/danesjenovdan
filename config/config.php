66d70c8b66de3d708324e4c5ddca0d92d861eb29
muki <filipdobranic@gmail.com>
2012-12-16 23:27:41 +0100

    htaccess

--- config/config.php
+++ config/config.php
@@ -31,5 +31,5 @@ session_start();
 
    $app_id = "212117025591104";
    $app_secret = "23f0eb6371165818dabb378b7fabbf7a";
-   $my_url = "http://localhost/~samo/danesjenovdan/login/facebook.php";
+   $my_url = "http://sect.io/login/facebook.php";
 
