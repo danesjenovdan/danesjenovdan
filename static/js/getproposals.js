efa895dc47b3c808d194191df73ddee561b15a28
muki <filipdobranic@gmail.com>
2012-12-16 17:27:45 +0100

    ajax example

--- static/js/getproposals.js
+++ static/js/getproposals.js
@@ -2,18 +2,7 @@ $(document).ready(function() {
 
 	$.getJSON('http://sect.io/danesjenovdan/ajax/list_proposals.php', function(data) {
 		for (var i=0; i<data.length; i++) {
-			$('.predlogi').add('
-				<div class="row predlog">
-					<div class="span4">
-						<p class="suggestiontitle">'+ data[i].title +'</p>
-					</div>
-				</div>	
-			');
+			$('#predlogi').append('<div class="row predlog"><div class="span4"><p class="suggestiontitle">'+ data[i].title +'</p></div><div class="span1"><p class="timestamp">'+data[i].timestamp+'</p></div><div class="span4"><div class="votebox"><div class="votefor">glasujza '+data[i].vote_plus+'</div><div class="voteagainst">glasujproti '+data[i].vote_minus+'</div></div></div><div class="span3"><div class="ihaveanargument">imam argument</div></div></div>');
 		}
-		$('#imepredloga').text(data[0].title);
-		$('#timestamppredloga').text(data[0].timestamp);
-		$('#voteplus').text(data[0].vote_plus);
-		$('#voteminus').text(data[0].vote_minus);
-	});
-	
+	});	
 });
