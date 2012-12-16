43e0ec28cb87f5eec06a210d549f6a70e9e71fca
muki <filipdobranic@gmail.com>
2012-12-16 18:35:23 +0100

    update on suggestion view

---
+++ static/js/suggestion.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	//buttons
+	$('.suggest').click(function() {
+		//TODO function to add suggestion
+	});
+	$('.suggestionup').click(function() {
+		//TODO function to vote for
+	});
+	$('.suggestiondown').click(function() {
+		//TODO function to vote against
+	});
+	$('.adddocument').click(function() {
+		//TODO function to add document
+	});
+	$('.argumentup').click(function() {
+		//TODO function to vote for
+	});
+	$('.argumentdown').click(function() {
+		//TODO function to vote against
+	});
+});
