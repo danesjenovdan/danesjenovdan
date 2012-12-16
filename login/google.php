9f5f70a1b682a8fe15450164f1ccaaad2593aac1
Marko Bratkovič <marko.bratkovic@gmail.com>
2012-12-16 22:36:14 +0100

    Nov filet

--- login/google.php
+++ login/google.php
@@ -1,10 +1,10 @@
 <?php
 
-include_once ('config/config.php');
+include_once ('../config/config.php');
 
 //var_dump($_SESSION);exit();
 # Logging in with Google accounts requires setting special identity, so this example shows how to do it.
-require 'lib/openid.php';
+require '../lib/openid.php';
 //try {
 	# Change 'localhost' to your domain name.
 	$openid = new LightOpenID($_SERVER['HTTP_HOST']);
@@ -28,7 +28,7 @@ require 'lib/openid.php';
 		{
 			//User logged in
 			$d = $openid->getAttributes();
-
+var_dump ($d);Exit();
 			$first_name = $d['namePerson/first'];
 			$last_name = $d['namePerson/last'];
 			$email = $d['contact/email'];
