68d9a7b4b9a3a4eea869cd14477726513e88d4cb
Bojan Rajh <schtr4jh@schtr4jh.net>
2012-12-20 00:55:33 +0100

    Moved to ../lib/user.php

--- ajax/isAuthorized.php
+++ ajax/isAuthorized.php
@@ -1,12 +1,10 @@
 <?php
+
+require_once ("../lib/user.php");
+
 header('Access-Control-Allow-Origin: *');
 session_start();
 
-if(isset($_SESSION['user'])){
-    $var=array('uid'=>$_SESSION['uid'],'name'=>$_SESSION['user']['name']." ".$_SESSION['user']['surname'], 'email'=>$_SESSION['user']['email']);
-} else {
-    $var=array('uid'=>-1);
-}
-   echo(json_encode($var));
+echo User::jsonEncodeUser();
 
 ?>
