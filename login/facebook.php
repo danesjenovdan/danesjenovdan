d778a375cf0af67b4c71a4b7878e698ad9fd64b4
Bojan Rajh <schtr4jh@schtr4jh.net>
2012-12-20 02:10:49 +0100

    Fix?

--- login/facebook.php
+++ login/facebook.php
@@ -3,14 +3,15 @@ require_once("../config/config.php");
 require_once("../lib/user.php");
 //session_start();
 
-if(!isset($_REQUEST["code"])){ 
+if(!isset($_REQUEST["code"])){
     $_SESSION['state'] = md5(uniqid(rand(), TRUE)); // CSRF protection
     
         $_SESSION['ref'] = $_SERVER['HTTP_REFERER'];
         
     $dialog_url = "https://www.facebook.com/dialog/oauth?client_id=" 
-       . $app_id . "&redirect_uri=" . urlencode($my_url . "?ref=" . $_SESSION['ref']) . "&state="
+       . $app_id . "&redirect_uri=" . urlencode($my_url) . "&state="
        . $_SESSION['state']."&scope=email";
+     //header("Location: " . $dialog_url);
      echo("<script> top.location.href='" . $dialog_url . "'</script>");
 }
 else
@@ -19,7 +20,7 @@ $code = $_REQUEST["code"];
 
 if($_SESSION['state'] && ($_SESSION['state'] === $_REQUEST['state'])) { 
     $token_url = "https://graph.facebook.com/oauth/access_token?"
-       . "client_id=" . $app_id . "&redirect_uri=" . urlencode($my_url . "?ref=" . $_SESSION['ref'])
+       . "client_id=" . $app_id . "&redirect_uri=" . urlencode($my_url)
        . "&client_secret=" . $app_secret . "&code=" . urlencode($code);
 
      //$response = file_get_contents($token_url);
@@ -30,7 +31,7 @@ if($_SESSION['state'] && ($_SESSION['state'] === $_REQUEST['state'])) {
             curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body 
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
             $response = curl_exec($ch); 
-     
+
      $params = null;
      parse_str($response, $params);
      $_SESSION['access_token'] = $params['access_token'];
@@ -44,6 +45,7 @@ if($_SESSION['state'] && ($_SESSION['state'] === $_REQUEST['state'])) {
             curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body 
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
             $response = curl_exec($ch);
+
     $user = json_decode($response);
             //$namesurname=explode(" ",$user->name);
 			$data = array(
@@ -63,7 +65,7 @@ if($_SESSION['state'] && ($_SESSION['state'] === $_REQUEST['state'])) {
 
     }
    else {
-    // echo("Login Failed...");
+     //echo("Login Failed...");
    }
 }
 ?>
