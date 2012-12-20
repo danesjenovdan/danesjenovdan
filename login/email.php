e5be86c0d1376aa3e8e9c8289ac1f165ce84c7f9
primoz klemensek <klemensek@gmail.com>
2012-12-20 23:10:12 +0100

    ček, če je user v bazi

--- login/email.php
+++ login/email.php
@@ -4,11 +4,13 @@ require_once ('../config/config.php');
 require_once ("../lib/user.php");
 
 if (isset($_POST['email']) && isset($_POST['name']) && isset($_POST['surname'])) {
-    $userID = User::addUser(array(
-        "email" => mysqli_real_escape_string($db, $_POST['email']),
-        "name" => mysqli_real_escape_string($db, $_POST['name']),
-        "surname" => mysqli_real_escape_string($db, $_POST['surname']),
-    ));
+	if(!User::getUserByArray(array ('email' => mysqli_real_escape_string($_POST['email']))) {
+	    $userID = User::addUser(array(
+	        "email" => mysqli_real_escape_string($db, $_POST['email']),
+	        "name" => mysqli_real_escape_string($db, $_POST['name']),
+	        "surname" => mysqli_real_escape_string($db, $_POST['surname']),
+	    ));
+	}
     
     User::login($userID);
 }
